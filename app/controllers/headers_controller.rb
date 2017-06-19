c9105c80803294df7a198bf6992f76758ec34cdc
Jeremy Shepherd <jeremylshepherd@gmail.com>
2017-06-19 18:35:40 +0000

    Working Microservice

---
+++ app/controllers/headers_controller.rb
@@ -0,0 +1,19 @@
+class HeadersController < ApplicationController
+    
+    def show
+        user_agent = request.user_agent
+        ip = request.ip
+        language = request.env['HTTP_ACCEPT_LANGUAGE']
+        hash = {:language => language, :software => software_data(user_agent), :ip => ip}
+        render json: hash, status: 200
+    end
+    
+    private
+    def software_data(str)
+        data = str.split('')
+        start = data.index('(') + 1
+        finish = data.index(')') - start
+        return data.slice(start, finish).join('')
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
+  get 'headers/whoami', to: 'headers#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
